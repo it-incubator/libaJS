adda1cc4cb86af89f813eea70313f18fd0e2ee3a
kuzyuberdin <kuzyuberdin@gmail.com>
2024-07-18 04:09:10 +0400

    update entire lib with code from Youtube video

--- liba/Liba.js
+++ liba/Liba.js
@@ -1,7 +1,7 @@
-function ensureStateAndChild(component) {
-    if (!component.localState) component.localState = {};
-    if (!component.localState.childComponents) component.localState.childComponents = [];
-
+function ensureChildren(parent) {
+    if (parent) {
+        if (!parent.childrenComponents) parent.childrenComponents = []
+    }
 }
 
 function propsTheSame(prevProps, newProps) {
@@ -27,92 +27,76 @@ function propsTheSame(prevProps, newProps) {
     return true;
 }
 
-class LibaError extends Error {
-    constructor(message) {
-        super(message)
-    }
-}
-
-function validateComponentFunction(componentFunction) {
-    if (typeof componentFunction.render !== 'function') {
-        throw new LibaError(`You must declare render method for your function component "${componentFunction.name}"
-
-Example: App.render = ({element, localState, props, liba}) => {}`)
-    }
-}
-
-function validateComponentInstance(component) {
-    if (!(component.element instanceof Node)) {
-        throw new LibaError(`Error in ${component.type.name} Each component function should return instance with element that is valid Node instance for example that created by document.createElement
-        {
-           ...
-           element: document.createElement('div')
-        }`)
-    }
-}
-
 
 export const Liba = {
-    create(componentFunction, props = {}, inner = {parent: null}) {
-        validateComponentFunction(componentFunction);
+    create(ComponentFunction, props = {}, {parent} = {parent: null}) {
+        const renderLiba = {
+            create: (ChildrenComponentFunction, props = {}) => {
+                componentInstance.childrenIndex++
 
-        const libaForComponent = {
-            refresh() {
-                render();
-            }
-        }
+                const alreadyExistedComponentInstance = componentInstance.childrenComponents?.[componentInstance.childrenIndex]
 
-        const libaForRender = {
-            create: (childComponentFunction, props = {}) => {
-
-                component.childIndex++
-
-
-                const existedChildComponent = component.localState.childComponents[component.childIndex]
-                if (existedChildComponent
-                    && propsTheSame(existedChildComponent.props, props)
-                ){
-                     return existedChildComponent;
+                if (alreadyExistedComponentInstance) {
+                    if (alreadyExistedComponentInstance.type === ChildrenComponentFunction) {
+                        if (propsTheSame(props, alreadyExistedComponentInstance.props)) {
+                            return alreadyExistedComponentInstance
+                        } else {
+                            alreadyExistedComponentInstance.props = props
+                            alreadyExistedComponentInstance.refresh()
+                            return alreadyExistedComponentInstance
+                        }
+                    } else {
+                        delete componentInstance.childrenComponents[componentInstance.childrenIndex]
+                    }
                 }
 
-                const childComponent = Liba.create(childComponentFunction, props, {parent: component})
+                const childInstance =  Liba.create(ChildrenComponentFunction, props, {parent: componentInstance})
 
-                ensureStateAndChild(component)
-                component.localState.childComponents[component.childIndex] = childComponent;
-
-                return childComponent
+                return childInstance;
             },
             refresh() {
-                render();
+                // todo: if element doesn't hav innerHTML??
+                componentInstance.element.innerHTML = ''
+
+                componentInstance.childrenComponents?.forEach(cc => cc.cleanup?.())
+                //componentInstance.childrenComponents = []
+
+                renderComponent();
             }
         }
+        const componentLiba = {
+            refresh: renderLiba.refresh
+        }
 
-        const component = componentFunction(props, {liba: libaForComponent})
-        component.type = componentFunction
+        const componentInstance = ComponentFunction(props, {liba: componentLiba})
 
-        validateComponentInstance(component)
+        componentInstance.type = ComponentFunction
 
-        parent.children?.push(component);
+        componentInstance.refresh = renderLiba.refresh
 
-        function render() {
-            component.element.innerHTML = ''
 
-            ensureStateAndChild(component)
+        if (parent) {
+            ensureChildren(parent)
+            parent.childrenComponents[parent.childrenIndex] = componentInstance
+        }
 
-            component.localState.childComponents.forEach(cc => cc.cleanup())
+        function renderComponent() {
 
-            component.childIndex = -1
+            componentInstance.childrenIndex = -1;
 
-            componentFunction.render({
-                element: component.element,
-                localState: component.localState,
-                props,
-                liba: libaForRender
+            ComponentFunction.render({
+                element: componentInstance.element,
+                localState: componentInstance.localState,
+                props: componentInstance.props,
+                liba: renderLiba
             })
         }
 
-        render()
+        renderComponent()
 
-        return component
+        return componentInstance
+    },
+    refresh() {
+
     }
 }
