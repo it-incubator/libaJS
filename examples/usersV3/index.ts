e4388f0750b9a0bd7c30636ac019296f2520b605
Nikita Pashin <135357793+Nikita-Pashin@users.noreply.github.com>
2025-01-31 21:58:11 +0500

    create fiber visualization in canvas (#14)
    Co-authored-by: Nikita <nikita.pashin.job@gmail.com>

--- examples/usersV3/index.ts
+++ examples/usersV3/index.ts
@@ -1,10 +1,12 @@
 import { App } from "./app";
 import { Liba } from "../../src/liba3/Liba";
 import { LibaDOM } from "../../src/liba3/LIbaDOM/LibaDOM";
+import { renderCanvasFiberTree } from "../../src/shared/renderCanvasFiberTree";
 
 const rootElement = LibaDOM.createRoot(document.getElementById('root'))
 
 const fiberNode = Liba.create(App);
+renderCanvasFiberTree(fiberNode);
 
 // @ts-ignore
 window.fiberNode = fiberNode;
