a7d09dbc9132d738614b96c9c9fe136bac4acac1
Nikita <nikita.pashin.job@gmail.com>
2025-02-09 21:23:13 +0500

    made liba4 with deleted renderLiba

---
+++ src/liba4/utils/create-fiber-node.ts
@@ -0,0 +1,48 @@
+import {StateWrapperWithSetter} from "./use-state.ts";
+
+export class FiberNode {
+     status: string;
+     cleanups: any[];
+     element: null;
+     parentElement: null;
+     props: any;
+     type: any;
+     children: any[];
+     child: FiberNode | null = null
+     sibling: FiberNode | null = null
+     parent: FiberNode | null = null
+
+     // сколько раз ueState вызывается в компоненте, столько будет здесь элементов
+     stateNode: StateWrapperWithSetter<any>[] = []
+     rendersCount = 0
+    /**
+     * текущий индекс вызова функции useState в текущем рендере
+     */
+     currentStateIndex = 0
+
+
+    constructor(
+        ComponentFunctionOrTagName: any, props: any) {
+        this.status = 'created'
+        this.cleanups = []
+
+        this.element = null
+        this.parentElement = null;
+        const {children, ...restProps} = props
+        this.props = restProps as any
+        this.type = ComponentFunctionOrTagName as any
+        this.children = []
+    }
+
+    pushState(stateWrapperWithSetter: StateWrapperWithSetter<any>) {
+        this.stateNode.push(stateWrapperWithSetter)
+    }
+    getState(){
+        const state = [this.stateNode[this.currentStateIndex][0].value, this.stateNode[this.currentStateIndex][1]];
+        this.currentStateIndex++;
+        return state;
+    }
+    resetStateIndex() {
+        this.currentStateIndex = 0;
+    }
+}
